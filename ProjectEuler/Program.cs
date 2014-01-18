500ab6b3882835c98bf641623b79d30ebace8052
ArturMroz <frost47@gmail.com>
2014-01-18 20:42:22 +0000

    Added solutions 1-5

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ProjectEuler
 {
@@ -10,6 +11,13 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
+            var clock = Stopwatch.StartNew();
+
+            var result = P005.Solve();
+            clock.Stop();
+
+            Console.WriteLine("Solution: {0}, calculated in: {1}", result, clock.Elapsed);
+            Console.ReadKey();
         }
     }
 }
