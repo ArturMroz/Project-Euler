d9cac01a022d62d362cbeeb3d0a6a028e54bc48f
ArturMroz <frost47@gmail.com>
2015-05-28 18:24:56 +0100

    + 36

--- ProjectEuler/P026.py
+++ ProjectEuler/P026.py
@@ -1,20 +1,23 @@
+# Find the value of d < 1000 for which 1/d contains the longest
+# recurring cycle in its decimal fraction part.
+
 import re
 import decimal as d
-d.setcontext(d.Context(prec=2000))
+
+lim = 1000
+d.setcontext(d.Context(prec=2 * lim))
 
 
 def longest_substring(s):
-    maxp = 0
+    maxlen = 0
     r = re.compile(r"(.+?)\1+")
     for match in r.finditer(s):
-        maxp = max(maxp, len(match.group(1)))
-
-    return maxp
+        maxlen = max(maxlen, len(match.group(1)))
+    return maxlen
 
 ans = 0, 0
-for i in range(2, 1000):
-    m = 1 / d.Decimal(i)
-    s = str(m)[2:]
+for i in range(2, lim):
+    s = str(1 / d.Decimal(i))[2:]
     curr = longest_substring(s)
     if curr > ans[1]:
         ans = i, curr
---
+++ ProjectEuler/P036.py
@@ -0,0 +1,6 @@
+# Find the sum of all numbers, less than one million,
+# which are palindromic in base 10 and base 2.
+
+
+def ispal(s): return s == s[::-1]
+print(sum(x for x in range(1000000) if ispal(str(x)) and ispal(bin(x)[2:])))
