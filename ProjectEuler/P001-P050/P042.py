9d18ecc5c1b73ea69ac6ce14aa5f12fe5c4f3e57
ArturMroz <frost47@gmail.com>
2015-05-30 14:39:49 +0100

    Moved problems into subfolders

---
+++ ProjectEuler/P001-P050/P042.py
@@ -0,0 +1,12 @@
+# How many triangle words are in input
+
+with open('../Resources/p042_words.txt') as f:
+    ans = 0
+    words = f.read().replace('"', '').split(',')
+    triangles = [x * (x + 1) // 2 for x in range(20)]
+
+    for word in words:
+        s = sum(ord(c) - 64 for c in word)
+        ans += s in triangles
+
+    print(ans)
--- ProjectEuler/P001-P050/P048.cs
+++ ProjectEuler/P001-P050/P048.cs
@@ -3,7 +3,7 @@ using System.Numerics;
 
 namespace ProjectEuler
 {
-    // Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+    // Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000
     class P048
     {
         public static long Solve()
