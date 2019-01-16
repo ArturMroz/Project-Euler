983c6267dbcf1ec4387ee5ccf8b11b5efa2a1b93
Artur Mroz <frost47@gmail.com>
2019-01-16 19:09:47 +0000

    added P031 in F#

---
+++ ProjectEuler/Problems 1 - 50/P031.fsx
@@ -0,0 +1,11 @@
+let coins = [ 1; 2; 5; 10; 20; 50; 100; 200 ]
+let n = 200;
+
+let state = Array.zeroCreate (n + 1)
+state.[0] <- 1;
+
+for coin in coins do
+    for j in coin..n do
+        state.[j] <- state.[j] + state.[j - coin]
+
+Array.last state
